1bf9c6200b08d0af47c385938217aebba2490fb1
Naia <naia.idgalo@gmail.com>
2020-10-08 21:41:24 +0200

    fn-21:41-tabledynamic

--- dynamictable/dynamictable.js
+++ dynamictable/dynamictable.js
@@ -1,28 +1,53 @@
 function funcMain(){
     document.getElementById("add_row").addEventListener("click", newRowTable);
+    document.getElementsByTagName("loans_table").addEventListener("click", '.fa-edit',deleteUser());
+    document.getElementsByTagName('loans_table').addEventListener("click", '.fa-trash',editUser())
+}
+function getRowSelected(objectPressed){
+    //Coger la linea que se esta eliminando
+    var a = objectPressed.parentNode.parentNode;
+    var name = a.getElementByTagName("td")[0].getElementsByTagName("p")[0].innerHTML;
+    var sn1 = a.getElementByTagName("td")[1].getElementsByTagName("p")[0].innerHTML;
+    var sn2 = a.getElementByTagName("td")[2].getElementsByTagName("p")[0].innerHTML;
+    var nick = a.getElementByTagName("td")[3].getElementsByTagName("p")[0].innerHTML;
+    var pass = a.getElementByTagName("td")[4].getElementsByTagName("p")[0].innerHTML;
+
+    var array_fila = [name, sn1, sn2, nick, pass];
+    return array_fila;
+}
+function deleteUser(){
+    //Guardar la referencia del objeto presionado
+    var _this = this;
+    //Obtener las filas de los datos de la fila que se va a eliminar
+    var array_fila = getRowSelected(_this);
 
 }
+function editUser(){
+    var _this = this;
+    var array_fila = getRowSelected(_this);
+    console.log(array_fila[0] + " - " + array_fila[1] + " - " + array_fila[2] + " - " + array_fila[3] + " - " + array_fila[4]);
+    //Codigo de editar una fila lo pueden agregar aqui
+}
 function newRowTable(){
-    /*Columnas*/
+    //Columnas
     var name = document.getElementById("name").value;
     var sn1 = document.getElementById("surname1").value;
     var sn2 = document.getElementById("surname2").value;
     var nick = document.getElementById("nick").value;
     var pass = document.getElementById("pass").value;
 
-    /*Tabla*/
+    //Tabla
     var tusers = document.getElementById("tableusers");
     var row = tusers.insertRow(0 + 1);
-    /*Número de celdas = Número de <td> introducido en HTML*/
+    //Número de celdas = Número de <td> introducido en HTML
     var cel1 = row.insertCell(0);
     var cel2 = row.insertCell(1);
     var cel3 = row.insertCell(2);
     var cel4 = row.insertCell(3);
     var cel5 = row.insertCell(4);
     var cel6 = row.insertCell(5);
-    var cel7 = row.insertCell(6);
 
-    /*Lo que vamos a tener en cada celda*/
+    //Lo que vamos a tener en cada celda
     cel1.innerHTML = '<p name="userName_f[]" class="non-margin">'+name+'</p>';
     cel2.innerHTML = '<p name="surname1_f[]" class="non-margin">'+sn1+'</p>';
     cel3.innerHTML = '<p name="surname2_f[]" class="non-margin">'+sn2+'</p>';
